f28fe5472baaf9c5380790a390debefd477ead61
Zaytsev Maxim <Maxim.Zaytsev@jetbrains.com>
2020-02-06 14:11:31 +0300

    attempt

--- src/main/java/com/github/dead_off/KotlinExample.kt
+++ src/main/java/com/github/dead_off/KotlinExample.kt
@@ -2,7 +2,7 @@ package com.github.dead_off
 
 class KotlinExample {
 
-    fun testOptionalParams(str1: String, option1: String? = null, option2: String? = null) {
+    fun testOptionalParams(str1: String) {
         print(str1)
     }
 }
